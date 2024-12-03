a7be1b0e0851ff4a1d24444519050348403b11df
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2024-12-03 15:14:28 +0530

    AOC 2024: Day 3

---
+++ 2024/3.py
@@ -0,0 +1,35 @@
+import re
+
+
+def get_multiplications(multiplications):
+    total_multiplications = 0
+    for multiplication in multiplications:
+        X, Y = map(int, re.findall("\d{1,3}", multiplication))
+        total_multiplications += X * Y
+    return total_multiplications
+
+
+def get_conditional_multiplications(instructions):
+    multiplications = []
+    is_multiplication_enabled = True
+    for instruction in instructions:
+        if instruction == "do()":
+            is_multiplication_enabled = True
+        elif instruction == "don't()":
+            is_multiplication_enabled = False
+        elif is_multiplication_enabled:
+            multiplications.append(instruction)
+
+    return get_multiplications(multiplications)
+
+
+memory = open("input.txt").read().replace('\n', ' ')
+
+# Part-1
+print('total_multiplications', re.findall("mul\(\d{1,3},\d{1,3}\)", memory))
+
+# Part-2
+print(
+    'total_conditional_multiplications',
+    get_conditional_multiplications(re.findall("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", memory))
+)
